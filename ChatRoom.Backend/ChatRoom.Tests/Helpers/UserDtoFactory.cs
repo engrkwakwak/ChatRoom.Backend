551b2c11d1b12761328413f46ffd724fff55e20d
ph1Bellie <ph1Bellie@obestsolutions.onmicrosoft.com>
2024-07-09 17:17:19 +0800

    Merge Carl_32237 UserDtoFactory to Bellie_32237 UserDtoFactory

--- ChatRoom.Backend/ChatRoom.Tests/Helpers/UserDtoFactory.cs
+++ ChatRoom.Backend/ChatRoom.Tests/Helpers/UserDtoFactory.cs
@@ -13,14 +13,107 @@ namespace ChatRoom.UnitTest.Helpers {
             };
         }        
 
-        public static UserForUpdateDto CreateUserForUpdateDto() {
-            return new UserForUpdateDto {
+        public static UserForUpdateDto CreateUserForUpdateDto()
+        {
+            return new UserForUpdateDto
+            {
                 Username = "testusername",
                 DisplayName = "Test Display Name",
                 Email = "test@email.com"
             };
         }
 
+        public static UserForUpdateDto CreateUserForUpdateDtoWithEmptyUsername()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "test@test.com",
+                Username = ""
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithUsernameGreaterThan20Characters()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "test@test.com",
+                Username = "123456789012345678901"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithEmptyDisplayName()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "",
+                Email = "test@test.com",
+                Username = "testusername"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithDisplayNameGreaterThan50Characters()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "123456789012345678901234567890123456789012345678901",
+                Email = "test@test.com",
+                Username = "testusername"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithEmptyEmail()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "",
+                Username = "testusername"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithEmailGreaterThan100Characters()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",
+                Username = "testusername"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithInvalidEmailFormat()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "1234567890123456789012345678901234",
+                Username = "testusername"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithAddressGreaterThan100Characters()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "test@email.com",
+                Username = "testusername",
+                Address = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
+            };
+        }
+
+        public static UserForUpdateDto CreateUserForUpdateDtoWithDisplayPictureUrlGreaterThan200Characters()
+        {
+            return new UserForUpdateDto
+            {
+                DisplayName = "Test Display Name",
+                Email = "test@email.com",
+                Username = "testusername",
+                DisplayPictureUrl = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890112345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901"
+            };
+}
         public static UserForUpdateDto CreateInvalidUserForUpdateDto() {
             return new UserForUpdateDto();
         }
@@ -46,5 +139,9 @@ namespace ChatRoom.UnitTest.Helpers {
             ];
         }
 
+        public static IEnumerable<UserDto> CreateEmptyListOfUserDto()
+        {
+            return [];
+        }
     }
 }
