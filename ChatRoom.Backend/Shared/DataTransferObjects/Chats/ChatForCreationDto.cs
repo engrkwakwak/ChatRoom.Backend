5a78ed5fd84d235a5c6b79a22913bfe2cdbc8e70
ph1Bellie <ph1Bellie@obestsolutions.onmicrosoft.com>
2024-07-10 10:07:09 +0800

    Update ChatForCreationDto validations

--- ChatRoom.Backend/Shared/DataTransferObjects/Chats/ChatForCreationDto.cs
+++ ChatRoom.Backend/Shared/DataTransferObjects/Chats/ChatForCreationDto.cs
@@ -5,12 +5,12 @@ namespace Shared.DataTransferObjects.Chats {
     public record ChatForCreationDto {
         [Required]
         //[MinimumValue(1)]
-        [Range(1, int.MaxValue, ErrorMessage = "SenderId must be a positive integer greater than zero.")]
+        [Range(1, 2, ErrorMessage = "SenderId must be in range 1 to 2.")]
         public int ChatTypeId { get; init; }
 
         [Required]
         //[MinimumValue(1)]
-        [Range(1, int.MaxValue, ErrorMessage = "SenderId must be a positive integer greater than zero.")]
+        [Range(1, 3, ErrorMessage = "StatusId must be in range 1 to 3.")]
         public int StatusId { get; init; }
 
         [MaxLength(50)]
