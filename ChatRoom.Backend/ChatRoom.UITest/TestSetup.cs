83e61657e4108197f80a34333ec99d47824d58ec
Carl <carlmarion.develops@gmail.com>
2024-07-15 19:14:32 +0800

    Setting up test environment.

---
+++ ChatRoom.Backend/ChatRoom.UITest/TestSetup.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoom.UITest {
+    public class TestSetup : IDisposable {
+        protected IWebDriver Driver { get; private set; }
+        public TestSetup() {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless"); // Run in headless mode
+
+            Driver = new ChromeDriver(chromeOptions);
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        public void Dispose() {
+            Driver.Quit();
+        }
+    }
+}
---
+++ ChatRoom.Backend/ChatRoom.UITest/Tests/ChatModuleTests.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ChatRoom.UITest.Tests {
+    public class ChatModuleTests : TestSetup {
+        private const string BaseUrl = "https://localhost:4000";
+
+    }
+}
