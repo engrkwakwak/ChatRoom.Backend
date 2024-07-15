f14120a5a670183f6ec4fc6bce33275bf16f4bcc
ph1Bellie <ph1Bellie@obestsolutions.onmicrosoft.com>
2024-07-15 11:36:31 +0800

    Created Initial UI Test

---
+++ ChatRoom.Backend/ChatRoom.UITest/Pages/SignInPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoom.UITest.Pages
+{
+    public class SignInPage
+    {
+        private readonly IWebDriver _driver;
+        private readonly string _rootUrl = Environments.FRONTEND_URI;
+        public SignInPage(IWebDriver driver) 
+        {
+            _driver = driver;
+        }
+
+        private IWebElement UserNameInput => _driver.FindElement(By.Id("username"));
+        private IWebElement PasswordInput => _driver.FindElement(By.Id("password"));
+        private IWebElement SignInButton => _driver.FindElement(By.Id("signin_btn"));
+
+        public void Navigate()
+        {
+            _driver.Navigate().GoToUrl($"{_rootUrl}/#/signin");
+        }
+
+        public void SubmitSignInForm()
+        {
+            SignInButton.Click();
+        }
+
+        public void PopulateForm(string username, string password)
+        {
+
+            UserNameInput.Click();
+            UserNameInput.Clear();
+            UserNameInput.SendKeys(username);
+
+            PasswordInput.Click();
+            PasswordInput.Clear();
+            PasswordInput.SendKeys(password);
+        }
+    }
+}
